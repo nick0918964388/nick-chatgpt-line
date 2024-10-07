8a11da1b41838474eefab70f430db7eb755a930e
nickyin <nickyin@tw.ibm.com>
2024-10-07 15:39:32 +0800

    t

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
